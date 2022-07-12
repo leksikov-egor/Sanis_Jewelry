fb6e38f6789945da85fd258cdfb3c8a96af45949
leksikov-egor <lex7prod@gmail.com>
2022-07-12 17:43:10 +0300

    start project

---
+++ gulp/tasks/js.js
@@ -0,0 +1,36 @@
+import webpack from 'webpack-stream';
+
+export const js = () => {
+    return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "JS",
+                message: "Error: <%= error.message %>"
+            }))
+        )
+        .pipe(webpack({
+            mode: app.isBuild ? 'production' : 'development',
+            output: {
+                filename: 'app.min.js',
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.m?js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: "babel-loader",
+                            options: {
+                                presets: [['@babel/preset-env', {
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                            }
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(app.gulp.dest(app.path.build.js))
+        .pipe(app.plugins.browsersync.stream());
+};
