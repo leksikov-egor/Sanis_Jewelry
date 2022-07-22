b4c9c6b507632a608037cd483c9ebe703d05b377
leksikov-egor <lex7prod@gmail.com>
2022-07-22 14:15:37 +0300

    add section cooperate

--- src/js/modules/slickSlider.js
+++ src/js/modules/slickSlider.js
@@ -1,5 +1,13 @@
 const slickSlider = () => {
     $(function () {
+        $(".headpresent__box").on("init", function(event, slick){
+            $(".headpresent__slider-count").text(parseInt(slick.currentSlide + 1) + ' | ' + slick.slideCount);
+        });
+        
+        $(".headpresent__box").on("afterChange", function(event, slick, currentSlide){
+            $(".headpresent__slider-count").text(parseInt(slick.currentSlide + 1) + ' | ' + slick.slideCount);
+        });
+
         $('.headpresent__box').slick({
             infinite: true,
             slidesToShow: 1,
@@ -18,6 +26,44 @@ const slickSlider = () => {
                     breakpoint: 1001,
                     settings: {
                         arrows: false
+                    }
+                },
+            ]
+        });
+
+        $('.cooperate__box').slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            prevArrow: `<button type="button" class="slick-arrow slick-prev"><svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 1L2 8L9 15" stroke="black" stroke-width="2"/><path d="M3 8H17" stroke="black" stroke-width="2"/></svg></button>`,
+            nextArrow: `<button type="button" class="slick-arrow slick-next"><svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 1L15 8L8 15" stroke="black" stroke-width="2"/><path d="M14 8H0" stroke="black" stroke-width="2"/></svg></button>`,
+            responsive: [
+                {
+                    breakpoint: 1701,
+                    settings: {
+                        slidesToShow: 4,
+                    }
+                },
+                {
+                    breakpoint: 1351,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 1051,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 751,
+                    settings: {
+                        slidesToShow: 1,
+                        dots: true,
+                        arrows: false,
+                        autoplay: true,
+                        autoplaySpeed: 2000,
                     }
                 },
             ]
